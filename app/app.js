1e422ace20642f099342a899afec2ad916efbc87
Dino Ferrari <DArachchi@gmail.com>
2017-09-21 23:47:54 -0500

    Refactored all module config code to app.js file

--- app/app.js
+++ app/app.js
@@ -9,5 +9,15 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+  
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,20 +2,11 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
-
 .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
     $http.get('teams/teams.json').then(function successCallback(response) {
       $scope.teams = response.data;
-      console.log(self.teams);
     },
     function errorCallback(response) {
       alert("Error on data loading:" + " " + response.status + ": " + response.statusText);
-      console.log(response);
     });
 }]);
