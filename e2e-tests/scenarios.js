82df7718b9559ef8ac585ed718b6bc94b49b7ae7
Dino Ferrari <DArachchi@gmail.com>
2017-09-21 22:43:15 -0500

    Updated E2E testing to pass with new text on view1

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* https://github.com/angular/protractor/blob/master/docs/toc.md */
-
 describe('my app', function() {
 
 
@@ -20,7 +18,7 @@ describe('my app', function() {
 
     it('should render view1 when user navigates to /view1', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+        toMatch('Formula 1 Teams');
     });
 
   });
